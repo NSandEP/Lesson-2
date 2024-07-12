5a999df64153a470f1b3b0f8556083f397d4c4bb
NSandEP <starart2004@mail.ru>
2024-07-12 16:30:47 +0300

    Add files via upload

---
+++ Homework2.py
@@ -0,0 +1,5 @@
+tskqnt = 12
+hrsqnt = 1.5
+coursename = 'Python'
+tsktmng = hrsqnt / tskqnt
+print('Курс:', coursename, ', всего задач:', tskqnt, ', затрачено часов:', hrsqnt, ', среднее время выполнения (ч):', tsktmng)
